21a0b2602782d918f53aa099c3c0ab5c8d807bdc
pouya-h <147872642+pouya-h@users.noreply.github.com>
2024-10-12 19:45:35 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let score = 0;
+const scoreDisplay = document.getElementById('score');
+const clickButton = document.getElementById('clickButton');
+
+clickButton.addEventListener('click', () => {
+    score++;
+    scoreDisplay.textContent = score;
+});
